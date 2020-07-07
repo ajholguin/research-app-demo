e73386535c1962b32e2e5177fe59ee260f046669
Andrew Holguin <ajholguin@ucdavis.edu>
2020-07-07 09:48:31 -0700

    switch to axios for api calls

--- scripts/research-app.js
+++ scripts/research-app.js
@@ -11,11 +11,12 @@ var py_app = new Vue({
 
     methods: {
         fetchData: function (x) {
-            this.$http.get(`https://7tw2vvqr25.execute-api.us-west-1.amazonaws.com/Prod/py_model/${x}/`)
-                      .then(response => {
-                        console.log(response);  // TODO: remove
-                        this.y = response.body.model_output.toFixed(2);
-                      })
+            axios
+                .get(`https://7tw2vvqr25.execute-api.us-west-1.amazonaws.com/Prod/py_model/${x}/`)
+                .then(response => {
+                    console.log(response);  // TODO: remove
+                    this.y = response.data.model_output.toFixed(2);
+                })
         }
     }
 });
@@ -34,11 +35,12 @@ var r_app = new Vue({
 
     methods: {
         fetchData: function (x) {
-            this.$http.get(`https://7tw2vvqr25.execute-api.us-west-1.amazonaws.com/Prod/r_model/${x}/`)
-                      .then(response => {
-                        console.log(response);  // TODO: remove
-                        this.y = response.body.model_output.toFixed(2);
-                      })
+            axios
+                .get(`https://7tw2vvqr25.execute-api.us-west-1.amazonaws.com/Prod/r_model/${x}/`)
+                .then(response => {
+                    console.log(response);  // TODO: remove
+                    this.y = response.data.model_output.toFixed(2);
+                })
         }
     }
 });
