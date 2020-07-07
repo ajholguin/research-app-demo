5a0ac18cad7bef89856013117b3f248e795cb637
Andrew Holguin <ajholguin@ucdavis.edu>
2020-07-06 20:17:41 -0700

    rename to py model

--- aws_lambdas/py_model/app.py
+++ aws_lambdas/py_model/app.py
@@ -40,10 +40,10 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET'
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET"
         },
         "body": json.dumps({
             "model_output": y,
--- scripts/research-app.js
+++ scripts/research-app.js
@@ -1,5 +1,5 @@
-var app = new Vue({ 
-    el: '#app',
+var py_app = new Vue({ 
+    el: '#py-app',
     data: {
         x: "0",
         y: ""
@@ -11,9 +11,14 @@ var app = new Vue({
 
     methods: {
         fetchData: function (x) {
-            this.$http.get(`https://7tw2vvqr25.execute-api.us-west-1.amazonaws.com/Prod/py_random/${x}/`)
+            this.$http.get(`https://7tw2vvqr25.execute-api.us-west-1.amazonaws.com/Prod/py_model/${x}/`)
                       .then(response => {
-                        console.log(response);
+                        console.log(response);  // TODO: remove
+                        this.y = response.body.model_output.toFixed(2);
+                      })
+        }
+    }
+});
                         this.y = response.body.model_output.toFixed(2);
                       })
         }
