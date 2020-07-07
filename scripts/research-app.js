99f04ec5d0c9250fc1cf7ce4813a075a8af71852
Andrew Holguin <ajholguin@ucdavis.edu>
2020-07-06 17:02:14 -0700

    connect input slider to AWS Lambda python function

--- scripts/research-app.js
+++ scripts/research-app.js
@@ -1,6 +1,21 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        value: 50
+        x: "0",
+        y: ""
+    },
+
+    created: function () {
+        this.fetchData("0");
+    },        
+
+    methods: {
+        fetchData: function (x) {
+            this.$http.get(`https://7tw2vvqr25.execute-api.us-west-1.amazonaws.com/Prod/py_random/${x}/`)
+                      .then(response => {
+                        console.log(response);
+                        this.y = response.body.model_output.toFixed(2);
+                      })
+        }
     }
 });
