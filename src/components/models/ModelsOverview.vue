efc04aead0ba6abc11f2d5729e20c0cab2964c39
Andrew Holguin <ajholguin@ucdavis.edu>
2020-08-12 11:26:09 -0700

    add mathjax to render model formula

--- src/components/models/ModelsOverview.vue
+++ src/components/models/ModelsOverview.vue
@@ -7,8 +7,20 @@
             <div class="panel panel--weighted-underline o-box--large">
               <h1 class="panel__title"><span class="panel__title--weighted">Models</span></h1>
               <p>
-                There are a couple of simple examples models...
+              This prototype app uses a simple linear function as an example:
               </p>
+              $$
+              y = 2x + 1 + \underbrace{\mathcal{N}(0, 1)}_{\text{Random noise}}
+              $$
+              This same model is implemented both in R and in Python. Each model
+              is deployed to an AWS Lambda function, and is callable via the
+              API:
+              <div class="sub-nav">
+                <ul class="menu">
+                  <li class='nav-item'><a class='nav-link' @click="pageSelected='pyModel'">Python</a></li>
+                  <li class='nav-item'><a class='nav-link' @click="pageSelected='rModel'">R</a></li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -18,8 +30,23 @@
 </template>
 
 <script>
+import { EventBus } from '../../main.js'
+
 export default {
-    
+  data: function() {
+    return {
+      pageSelected: ''
+    }
+  },
+  watch: {
+    pageSelected: function() {
+      EventBus.$emit('pageChanged', this.pageSelected)
+    }
+  },
+  mounted() {
+    window.MathJax.startup.document.reset()
+    window.MathJax.startup.document.render()
+  }
 }
 </script>
 
